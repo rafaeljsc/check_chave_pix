c9ece73dd5454ba46ecdc8b5313431fa1b35350e
Rafael Costa <72453565+rafaeljsc@users.noreply.github.com>
2024-01-28 02:26:22 -0300

    Add files via upload

---
+++ check_chave_pix.py
@@ -0,0 +1,30 @@
+import os
+from validate_docbr import CPF, CNPJ
+os.system('cls')
+
+def chavePix(chave):
+  chave = chave.strip()
+  if '@' in chave:
+    return('Tipo: E-mail')
+  
+
+  elif len(chave) == 36 and len(chave.split('-')) == 5:
+    return('Tipo: Chave aleatória')
+  
+  elif len(chave) == 11 and chave.isdigit():
+    if CPF().validate(chave):
+      return('Tipo: CPF')
+    else:
+      return('Tipo: Celular')
+  
+  elif CNPJ().validate(chave):
+      return('Tipo: CNPJ')
+  
+  else:
+    return('O pix informado é inválido.')
+
+while True:
+  os.system('cls')
+  chave = input('Chave pix: ')
+  print(chavePix(chave))
+  input('')
